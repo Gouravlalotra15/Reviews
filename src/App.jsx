2a1a8deefa97ee76ba81936d9bac8558f9503b19
Gourav <gouravlalotraa@gmail.com>
2023-08-20 15:41:56 +0530

    updated the app component

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,23 @@ import people from './data'
 import { FaChevronCircleLeft,FaChevronCircleRight,FaQuoteRight} from "react-icons/fa";
 const App = () => {
   const[index,setIndex]=useState(0);
-  const{name,job,image,text}=people[0];
+  const{name,job,image,text}=people[index];
  
+  // click event functions
+  
+  const nextPerson=()=>{
+    setIndex((currentIndex)=>(currentIndex+1)%people.length);
+  };
+  const prevPerson=()=>{setIndex((index)=>(((index-1)+people.length)%people.length))};
+
+  const randPerson=()=>{
+    let newIndex=Math.floor(Math.random()*people.length);
+    if(index===newIndex)
+      newIndex=index+1;
+    console.log(newIndex);
+    setIndex(newIndex);
+  }
+  // output window
   return (
     <main>
       <article className="review">
@@ -15,6 +30,12 @@ const App = () => {
         <h4 className="author">{name}</h4>
         <p className="job">{job}</p>
         <p className="info">{text}</p>
+
+        <div className="btn-container">
+          <button type="submit" className="prev-btn" onClick={prevPerson}><FaChevronCircleLeft/></button>
+          <button  className="next-btn" onClick={nextPerson}><FaChevronCircleRight/></button>
+        </div>
+        <button className="btn btn-hipster" onClick={randPerson}>Shuffle</button>
       </article>
     </main>
   );
